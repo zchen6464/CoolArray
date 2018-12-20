a4bc79952fa9f964258066e84bf53d06acc6687f
Z Chen <zchen6464@bths.edu>
2018-12-20 10:54:04 -0500

    Update Successors.java

--- src/com/company/Successors.java
+++ src/com/company/Successors.java
@@ -2,5 +2,32 @@ package com.company;
 
 public class Successors {
 
-    public static position findPosition(int[][] arr, int num)
+    public static Position findPosition(int[][] arr, int num)
+    {
+        for( int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr[i].length; j++)
+            {
+                if(arr[i][j] == num)
+                {
+                    return new Position(i,j);
+                }
+            }
+        }
+        return null;
+    }
+    
+    public static Position[][] getSuccessorArray(int[][] arr)
+    {
+        Position[][] intArr = new Position[arr.length][arr[0].length];
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr[i].length; j++)
+            {
+                intArr[i][j] = findPosition(arr,arr[i][j]+1);
+            }
+        }
+        return intArr;
+    }
 }
