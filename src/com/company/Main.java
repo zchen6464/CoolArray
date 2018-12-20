c5d0505450ebcbeb44f5e1d9e924a2b6ff1e459b
Z Chen <zchen6464@bths.edu>
2018-12-20 10:56:44 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[][] arr = {
+		    {10,15,13,8},
+		    {12,6,9,7},
+		    {17,11,16,14}
+	    };
+	    
+	    Position a = Successors.findPosition(7,arr);
+	    System.out.println("Row of 7: " + a.row);
+	    System.out.println("Column of 7: " + a.col);
     }
 }
