bdd9e5b338178e69f5cd7d6510df270b63fe61f8
BT_1N3_28@bktech.local <BT_1N3_28@bktech.local>
2018-12-20 10:42:53 -0500

    Initial commit

---
+++ src/com/company/Position.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Position {
+    private int row, col;
+
+    public Position(int r, int c)
+    {
+        this.row = r;
+        this.col = c;
+    }
+
+}
---
+++ src/com/company/Successors.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Successors {
+
+    public static position findPosition(int[][] arr, int num)
+}
